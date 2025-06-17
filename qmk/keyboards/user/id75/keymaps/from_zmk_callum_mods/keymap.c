90973667c4a859cb96f49821f310de278cd182c0
Romain Sabathe <romain.sabathe@onfido.com>
2025-06-17 07:38:47 +0100

    QMK: custom tri-layer functionality for NORMAL and MAC modes

--- qmk/keyboards/user/id75/keymaps/from_zmk_callum_mods/keymap.c
+++ qmk/keyboards/user/id75/keymaps/from_zmk_callum_mods/keymap.c
@@ -18,10 +18,10 @@
 #include "sendstring_us_international.h"
 #include "oneshot.h"
 
-#define LA_SYM MO(SYM)
-#define LA_NAV MO(NAV)
-#define LA_MAC_SYM MO(MAC_SYM)
-#define LA_MAC_NAV MO(MAC_NAV)
+#define LA_SYM KC_TRI_LOWER
+#define LA_NAV KC_TRI_UPPER
+#define LA_MAC_SYM LA_SYM
+#define LA_MAC_NAV LA_NAV
 
 // Layer definitions
 enum layers {
@@ -39,20 +39,19 @@ enum layers {
 enum custom_keycodes {
     KC_NAVSP = SAFE_RANGE,
     KC_MEDEL,
-    KC_SYMSP
-};
+    KC_SYMSP,
+    KC_TRI_LOWER,
+    KC_TRI_UPPER,
 
-
-enum keycodes {
     // Custom oneshot mod implementation with no timers.
-    OS_SHFT = SAFE_RANGE,
+    OS_SHFT,
     OS_CTRL,
     OS_ALT,
     OS_CMD,
-
     SW_WIN,  // Switch to next window         (cmd-tab)
     SW_LANG, // Switch to next input language (ctl-spc)
 };
+
 
 
 // tap dance
@@ -77,7 +76,7 @@ combo_t key_combos[] = {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ortho_5x15(
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,          KC_5,    KC_6,    KC_7,    KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC,
+        QK_BOOT,  KC_1,    KC_2,    KC_3,    KC_4,          KC_5,    KC_6,    KC_7,    KC_8,   KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_BSPC,
         KC_TAB,  KC_B,    KC_L,    KC_D,    KC_C,          KC_V,    KC_VOLD, KC_VOLU, KC_J,   KC_F,    KC_O,    KC_U,    KC_COMM, KC_LBRC, KC_RBRC,
         KC_ESC,  KC_N,    KC_R,    KC_T,    KC_S,          KC_G,    _______, _______, KC_Y,   KC_H,    KC_A,    KC_E,    KC_I,    KC_SLSH, KC_ENT,
         KC_LSFT, KC_X,    KC_Q,    KC_M,    KC_W,          KC_Z,    _______, _______, KC_K,   KC_P,    KC_QUOT, KC_SCLN, KC_DOT,  KC_RSFT, KC_ENT,
@@ -89,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,          LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), LCTL(KC_Y), _______,    _______,    LCTL(LSFT(KC_L)), LALT(KC_TAB), LCTL(KC_TAB), LCTL(LSFT(KC_TAB)), LALT(LSFT(KC_TAB)), LCTL(LALT(LSFT(KC_L))), _______,
         LALT(LSFT(KC_Q)), OS_CMD,     OS_ALT,     OS_SHFT,    OS_CTRL,    KC_ESC,     _______,    _______,    KC_ENT,           KC_LEFT,      KC_DOWN,      KC_UP,              KC_RGHT,            KC_DEL,                 LALT(KC_ENT),
         _______,          LALT(KC_R), LALT(KC_D), LCTL(KC_A), LALT(KC_F), LALT(KC_V), _______,    _______,    CW_TOGG,          KC_HOME,      KC_PGDN,      KC_PGUP,            KC_END,             KC_CAPS,                _______,
-        QK_BOOT,          _______,    _______,    _______,    _______,    _______,    _______,    MO(NUM),    _______,          _______,      _______,      _______,            _______,            _______,                _______
+        QK_BOOT,          _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,          _______,      _______,      _______,            _______,            _______,                _______
     ),
 
     [SYM] = LAYOUT_ortho_5x15(
@@ -97,7 +96,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_LCBR,        KC_LBRC,  KC_LPRN,  KC_DQUO,  KC_PIPE, _______, _______, KC_GRAVE,    KC_QUOTE, KC_RPRN, KC_RBRC,    KC_RCBR,      _______, _______,
         _______, KC_EXCLAIM,     KC_PLUS,  KC_MINUS, KC_EQUAL, KC_ESC,  _______, _______, KC_ENT,      OS_CTRL,  OS_SHFT, OS_ALT,     OS_CMD,       KC_ENT,  _______,
         _______, KC_KP_ASTERISK, KC_TILDE, KC_SLASH, KC_HASH,  KC_BSLS, _______, _______, KC_QUESTION, KC_SPC,   KC_AT,   KC_PERCENT, KC_AMPERSAND, _______, _______,
-        _______, _______,        _______,  _______,  QK_AREP,  QK_REP,  MO(NUM), _______, _______,     _______,  _______, _______,    _______,      _______, QK_BOOT
+        _______, _______,        _______,  _______,  QK_AREP,  QK_REP,  _______, _______, _______,     _______,  _______, _______,    _______,      _______, QK_BOOT
     ),
 
     [NUM] = LAYOUT_ortho_5x15(
@@ -113,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_B,    KC_L,    KC_D,    KC_C,          KC_V,    KC_VOLD,    KC_VOLU,    KC_J,   KC_F,    KC_O,    KC_U,    KC_COMM, KC_LBRC, KC_RBRC,
         KC_ESC,  KC_N,    KC_R,    KC_T,    KC_S,          KC_G,    _______,    _______,    KC_Y,   KC_H,    KC_A,    KC_E,    KC_I,    KC_SLSH, KC_ENT,
         KC_LSFT, KC_X,    KC_Q,    KC_M,    KC_W,          KC_Z,    _______,    _______,    KC_K,   KC_P,    KC_QUOT, KC_SCLN, KC_DOT,  KC_RSFT, KC_ENT,
-        KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, OSM(MOD_LSFT), KC_BSPC, LA_MAC_NAV, LA_MAC_SYM, KC_SPC, KC_UNDS, KC_RALT, KC_RGUI, KC_RCTL, KC_RCTL, _______
+        KC_LCTL, KC_LGUI, KC_LALT, KC_LALT, OSM(MOD_LSFT), KC_BSPC, LA_NAV, LA_SYM, KC_SPC, KC_UNDS, KC_RALT, KC_RGUI, KC_RCTL, KC_RCTL, _______
     ),
 
     [MAC_NAV] = LAYOUT_ortho_5x15(
@@ -121,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,          LCMD(KC_Z), LCMD(KC_X), LCMD(KC_C), LCMD(KC_V), LCMD(KC_Y), _______,    _______,     LCMD(LSFT(KC_L)), LALT(KC_TAB),  LCTL(KC_TAB), LCTL(LSFT(KC_TAB)), LALT(LSFT(KC_TAB)), LCTL(LALT(LSFT(KC_L))), _______,
         LALT(LSFT(KC_Q)), OS_CTRL,    OS_ALT,     OS_SHFT,    OS_CMD,     KC_ESC,     _______,    _______,     KC_ENT,           KC_LEFT,       KC_DOWN,      KC_UP,              KC_RGHT,            KC_DEL,                 LALT(KC_ENT),
         _______,          _______,    _______,    LCMD(KC_A), LALT(KC_F), _______,    _______,    _______,     _______,          LCTL(KC_LEFT), KC_PGDN,      KC_PGUP,            LCTL(KC_RGHT),      KC_CAPS,                _______,
-        QK_BOOT,          _______,    _______,    _______,    _______,    _______,    _______,    MO(MAC_NUM), _______,          _______,       _______,      _______,            _______,            _______,                _______
+        QK_BOOT,          _______,    _______,    _______,    _______,    _______,    _______,    _______, _______,          _______,       _______,      _______,            _______,            _______,                _______
     ),
 
     [MAC_SYM] = LAYOUT_ortho_5x15(
@@ -129,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_LCBR,        KC_LBRC,  KC_LPRN,  KC_DQUO,  KC_PIPE, _______,     _______, KC_GRAVE,    KC_QUOTE, KC_RPRN, KC_RBRC,    KC_RCBR,      _______, _______,
         _______, KC_EXCLAIM,     KC_PLUS,  KC_MINUS, KC_EQUAL, KC_ESC,  _______,     _______, KC_ENT,      OS_CMD,   OS_SHFT, OS_ALT,     OS_CTRL,      KC_ENT,  _______,
         _______, KC_KP_ASTERISK, KC_TILDE, KC_SLASH, KC_HASH,  KC_BSLS, _______,     _______, KC_QUESTION, KC_SPC,   KC_AT,   KC_PERCENT, KC_AMPERSAND, _______, _______,
-        _______, _______,        _______,  _______,  QK_AREP,  QK_REP, MO(MAC_NUM), _______, _______,     _______,  _______, _______,    _______,      _______, QK_BOOT
+        _______, _______,        _______,  _______,  QK_AREP,  QK_REP, _______, _______, _______,     _______,  _______, _______,    _______,      _______, QK_BOOT
     ),
 
     [MAC_NUM] = LAYOUT_ortho_5x15(
@@ -145,8 +144,6 @@ bool is_oneshot_cancel_key(uint16_t keycode) {
     switch (keycode) {
     case LA_SYM:
     case LA_NAV:
-    case LA_MAC_SYM:
-    case LA_MAC_NAV:
         return true;
     default:
         return false;
@@ -157,8 +154,6 @@ bool is_oneshot_ignored_key(uint16_t keycode) {
     switch (keycode) {
     case LA_SYM:
     case LA_NAV:
-    case LA_MAC_SYM:
-    case LA_MAC_NAV:
     case KC_LSFT:
     case OS_SHFT:
     case OS_CTRL:
@@ -178,6 +173,10 @@ oneshot_state os_ctrl_state = os_up_unqueued;
 oneshot_state os_alt_state = os_up_unqueued;
 oneshot_state os_cmd_state = os_up_unqueued;
 
+bool is_mac_mode = false;  // Start in normal mode by default.
+bool tri_lower_pressed = false;  // Whether the KC_TRI_LOWER key is pressed
+bool tri_upper_pressed = false;  // Whether the KC_TRI_UPPER key is pressed.
+
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     update_oneshot(
         &os_shft_state, KC_LSFT, OS_SHFT,
@@ -195,6 +194,67 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         &os_cmd_state, KC_LCMD, OS_CMD,
         keycode, record
     );
+
+    switch (keycode) {
+        // Keep track of whether we're using normal mode or MAC mode
+        case DF(BASE):
+            if (record->event.pressed) {
+                is_mac_mode = false;
+            }
+            return false;
+        case DF(MAC_BASE):
+            if (record->event.pressed) {
+                is_mac_mode = true;
+            }
+            return false;
+
+        // Custom TRI_LAYER functionality that will work across the "normal" level and the "MAC" level
+        case KC_TRI_LOWER:
+            tri_lower_pressed = record->event.pressed;
+            break;
+        case KC_TRI_UPPER:
+            tri_upper_pressed = record->event.pressed;
+            break;
+    }
+
+    // Determine which layers to activate (custom TRI_LAYER)
+    if (tri_lower_pressed && tri_upper_pressed) {
+        if (!is_mac_mode) {
+            layer_on(NUM);
+        } else {
+            layer_on(MAC_NUM);
+        }
+    } else {
+        layer_off(NUM);
+        layer_off(MAC_NUM);
+        if (tri_lower_pressed) {
+            layer_on(is_mac_mode ? MAC_SYM : SYM);
+        } else {
+            layer_off(SYM);
+            layer_off(MAC_SYM);
+        }
+        if (tri_upper_pressed) {
+            layer_on(is_mac_mode ? MAC_NAV : NAV);
+        } else {
+            layer_off(NAV);
+            layer_off(MAC_NAV);
+        }
+    }
+
+    // Handle mode switching in the NUM layer
+    if (IS_LAYER_ON(NUM) || IS_LAYER_ON(MAC_NUM)) {
+        if (keycode == DF(MAC_BASE) && record->event.pressed) {
+            is_mac_mode = true;
+            layer_off(NUM);
+            layer_on(MAC_BASE);
+            return false;
+        } else if (keycode == DF(BASE) && record->event.pressed) {
+            is_mac_mode = false;
+            layer_off(MAC_NUM);
+            layer_on(BASE);
+            return false;
+        }
+    }
 
     return true;
 }
